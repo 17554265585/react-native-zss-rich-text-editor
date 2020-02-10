8a5714049e6e56b6931cfba52b0bdfb0f3cb2a81
zhangchao <zhangchao@dengdengfu.com>
2020-02-10 15:10:50 +0800

    s

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import RichTextEditor from './src/RichTextEditor';
-import RichTextToolbar from './src/RichTextToolbar';
 import {actions} from './src/const';
 
 module.exports = {
-  RichTextEditor, RichTextToolbar, actions
+  RichTextEditor, actions
 }
